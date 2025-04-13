59b65cd76a3abe2904de1041e4a7ad9ce5341612
TbOMu4 <artemtumoshenko@gmail.com>
2025-04-13 20:27:38 +0300

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/react-ithillel/homework-25.1/",
+  base: "/react-ithillel/",
   plugins: [react(), tailwindcss()],
 });
