9b8f5167f2cb62b5fb7e9739f0504b47a7feb221
TbOMu4 <artemtumoshenko@gmail.com>
2025-04-13 20:14:07 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import VotingEmoticon from "./component/VotingEmoticon";
+
+function App() {
+  return (
+    <div>
+      <VotingEmoticon />
+    </div>
+  );
+}
+
+export default App;
